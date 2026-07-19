24db69434af683f59a5a223b94c777937668537a
Agus Risky <240040110@stikom-bali.ac.id>
2026-07-19 22:45:34 +0700

    new

--- pert7.py
+++ pert7.py
@@ -1,30 +1,150 @@
+# # # =========================
+# # # HITUNG GAJI
+# # # =========================
+
+# # # Nama : Risky
+
+# # # Gaji Pokok : 5000000
+
+# # # Bonus : 1000000
+
+# # # Total Gaji : 6000000
+
+# # def garis():
+# #     print ("====================")
+
+# # garis()
+# # print ("SELAMAT DATANG")
+# # garis()
+
+# # nama = input("Masukan Nama Anda : ",)
+# # umur = int(input("Masukan Umur Anda : "))
+# # alamat = input("Masukan aamat anda : ")
+
+# # garis()
+# # print ("BIODATA")
+# # garis()
+
+# # print ("Nama : ", nama)
+# # print ("Umur : ", umur)
+# # print ("Alamat : ", alamat)
+
+# SOAL 17
 # =========================
-# HITUNG GAJI
+#     DATA PEGAWAI
+# =========================
+
+# 1. Tambah Data
+# 2. Tampilkan Data
+# 3. Cek Gaji
+# 4. Keluar
+
+# Pilih : 1
+
+# Nama : Risky
+# NIP : 240040110
+# Gaji : 6000000
+
+# Data berhasil ditambahkan
+
+# Pilih : 2
+
+# =========================
+# DATA PEGAWAI
 # =========================
 
 # Nama : Risky
+# NIP : 240040110
+# Gaji : 6000000
 
-# Gaji Pokok : 5000000
+# Pilih : 3
 
-# Bonus : 1000000
+# Gaji : 6000000
+# Status : Gaji Tinggi
 
-# Total Gaji : 6000000
+# Pilih : 4
 
-def garis():
-    print ("====================")
+# Program selesai
 
-garis()
-print ("SELAMAT DATANG")
-garis()
+print("=========================")
+print("    DATA PEGAWAI")
+print("=========================")
 
-nama = input("Masukan Nama Anda : ",)
-umur = int(input("Masukan Umur Anda : "))
-alamat = input("Masukan aamat anda : ")
+nama = ""
+nip = ""
+gaji = 0
 
-garis()
-print ("BIODATA")
-garis()
 
-print ("Nama : ", nama)
-print ("Umur : ", umur)
-print ("Alamat : ", alamat)
+def tambah_data():
+
+    global nama
+    global nip
+    global gaji
+
+    nama = input("Masukan Nama : ")
+    nip = input("Masukan NIP : ")
+    gaji = int(input("Masukan Gaji : "))
+
+    print("Data berhasil ditambahkan")
+
+
+def tampil_data():
+
+    print("=========================")
+    print("DATA PEGAWAI")
+    print("=========================")
+
+    print("Nama :", nama)
+    print("NIP :", nip)
+    print("Gaji :", gaji)
+
+
+def cek_gaji():
+
+    if gaji >= 5000000:
+        return "Gaji Tinggi"
+
+    else:
+        return "Gaji Rendah"
+
+
+def menu():
+
+    print("\n1. Tambah Data")
+    print("2. Tampilkan Data")
+    print("3. Cek Gaji")
+    print("4. Keluar")
+
+
+jalan = True
+
+while jalan:
+
+    menu()
+
+    pilihan = input("Pilih : ")
+
+    if pilihan == "1":
+
+        tambah_data()
+
+    elif pilihan == "2":
+
+        tampil_data()
+
+    elif pilihan == "3":
+
+        hasil = cek_gaji()
+
+        print("Gaji :", gaji)
+        print("Status :", hasil)
+
+    elif pilihan == "4":
+
+        print("Program selesai")
+
+        jalan = False
+
+    else:
+
+        print("Menu tidak tersedia")
