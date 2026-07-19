552f72e09033bd007736e0775080a94f6df0a5a2
Agus Risky <240040110@stikom-bali.ac.id>
2026-07-19 22:10:48 +0700

    baru

---
+++ pert7.py
@@ -0,0 +1,30 @@
+# =========================
+# HITUNG GAJI
+# =========================
+
+# Nama : Risky
+
+# Gaji Pokok : 5000000
+
+# Bonus : 1000000
+
+# Total Gaji : 6000000
+
+def garis():
+    print ("====================")
+
+garis()
+print ("SELAMAT DATANG")
+garis()
+
+nama = input("Masukan Nama Anda : ",)
+umur = int(input("Masukan Umur Anda : "))
+alamat = input("Masukan aamat anda : ")
+
+garis()
+print ("BIODATA")
+garis()
+
+print ("Nama : ", nama)
+print ("Umur : ", umur)
+print ("Alamat : ", alamat)
