9661116630445cde82feaefde2075ccc4b85d8b6
Agus Risky <240040110@stikom-bali.ac.id>
2026-07-22 20:36:45 +0700

    tambahan1

--- pert9.py
+++ pert9.py
@@ -5,9 +5,33 @@
 
 # Halo, selamat datang Risky
 
-def login(nama):
-    print ("Halo, selamat datang", nama)
+# def login(nama):
+#     print ("Halo, selamat datang", nama)
 
-nama = input("Masukan Nama :")
+# nama = input("Masukan Nama :")
 
-login(nama)
+# login(nama)
+
+
+# SOAL 2
+# Masukkan Nama : Risky
+# Masukkan Umur : 19
+
+# ====================
+# BIODATA
+# ====================
+
+# Nama : Risky
+# Umur : 19
+
+def biodata(nama, umur):
+    print ("==================")
+    print ("      BIODATA     ")
+    print ("==================")
+    print ("Nama : ", nama)
+    print ("Umur : ", umur)
+
+nama = input ("Masukan Nama :")
+umur = int(input ("Masukan Umur :"))
+
+biodata(nama, umur)
