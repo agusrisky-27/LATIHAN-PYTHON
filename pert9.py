14908da4b22cc973c308200d42a6a4efdbc14a70
Agus Risky <240040110@stikom-bali.ac.id>
2026-07-22 20:07:20 +0700

    materi baru

---
+++ pert9.py
@@ -0,0 +1,13 @@
+# MATERI PARAETER
+
+# SOAL 1
+# Masukkan Nama : Risky
+
+# Halo, selamat datang Risky
+
+def login(nama):
+    print ("Halo, selamat datang", nama)
+
+nama = input("Masukan Nama :")
+
+login(nama)
